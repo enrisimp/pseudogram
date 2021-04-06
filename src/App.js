e4be3b0b77f1476071ccb740f6c2f71dc954ea63
Enrique Escalante <internet_surfer1@yahoo.com>
2021-04-06 18:29:47 -0300

    Dia2 con error de Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Pseudogram</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.renderLoginButton = this.renderLoginButton.bind(this);
+  }
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState = ({ user });
+    });
+  }
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton() {
+    //si el usuario esta logueado
+    if (this.state.user) {
+      return (
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      );
+    } else {
+      // si no esta
+      return (
+        <button onClick={this.handleAuth}>Login con Google</button>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h2>Pseudogram</h2>
+          <p className="App-intro">
+            {this.renderLoginButton()}
+          </p>
+        </header>
+      </div>
+    );
+  }
+
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBoKI4UkwOZYICoJv7Qjb6INAOoGKmWMvo",
+    authDomain: "pseudogram-671ae.firebaseapp.com",
+    projectId: "pseudogram-671ae",
+    storageBucket: "pseudogram-671ae.appspot.com",
+    messagingSenderId: "61343491084",
+    appId: "1:61343491084:web:ea1789739597b227daba29",
+    measurementId: "G-PDDBHXHBPC"
+});
 
 ReactDOM.render(
   <React.StrictMode>
